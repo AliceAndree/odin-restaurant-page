3c646dfa5277126c492dbed32da4c0a6fcc4a378
AliceAndree <thiel.alice@gmail.com>
2025-02-20 13:41:15 +0100

    Homepage Menu Section

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,62 +1,9 @@
-import logoSvg from "../../assets/logo.svg";
-import chevronSvg from "../../assets/chevron.svg";
-import downArrowSvg from "../../assets/down-arrow.svg";
+import HeroSection from "../../functions/heroSection";
+import MenuSection from "../../functions/menuSection";
 
 const createHomePage = () => {
-  const heroSection = document.querySelector("#hero-section");
-  const logo = document.createElement("img");
-  const chevronOne = document.createElement("img");
-  chevronOne.classList.add("chevron");
-  const chevronTwo = document.createElement("img");
-  chevronTwo.classList.add("chevron");
-  const downArrow = document.createElement("img");
-  downArrow.id = "down-arrow";
-  const sloganList = document.createElement("ul");
-  const slogans = [
-    "comfort",
-    chevronOne,
-    "classics",
-    chevronTwo,
-    "curiosities",
-  ];
-
-  logo.id = "logo";
-  logo.src = logoSvg;
-  heroSection.appendChild(logo);
-
-  for (let i = 0; i < slogans.length; i++) {
-    const slogan = document.createElement("li");
-
-    if (typeof slogans[i] === "string") {
-      slogan.textContent = slogans[i];
-    } else {
-      slogan.appendChild(slogans[i]);
-    }
-    sloganList.appendChild(slogan);
-  }
-
-  heroSection.appendChild(sloganList);
-  const chevrons = document.querySelectorAll(".chevron");
-
-  chevrons.forEach((chevron) => {
-    chevron.src = chevronSvg;
-  });
-
-  downArrow.src = downArrowSvg;
-  heroSection.appendChild(downArrow);
-
-  downArrow.addEventListener("click", () => {
-    const menuSection = "#menu-section";
-    window.location.href = menuSection;
-  });
-
-  document.addEventListener("scroll", () => {
-    if (window.scrollY > 450) {
-      downArrow.classList.add("hidden");
-    } else {
-      downArrow.classList.remove("hidden");
-    }
-  });
+  HeroSection();
+  MenuSection();
 };
 
 export default createHomePage;
---
+++ src/functions/menuSection.js
@@ -0,0 +1,50 @@
+import burgerImg from "../assets/burger.jpg";
+import menuBorderImg from "../assets/menu-border.png";
+
+const createMenuSection = () => {
+  const menuSection = document.querySelector("#menu-section");
+  const menuImage = document.createElement("div");
+  const menuPresentation = document.createElement("div");
+
+  menuImage.src = burgerImg;
+  menuImage.id = "menu-image";
+  menuImage.style.setProperty(`--burger-image`, `url("${burgerImg}")`);
+  menuSection.appendChild(menuImage);
+
+  const menuTitle = document.createElement("h2");
+  menuTitle.textContent = "Our Menus";
+
+  const menuParagraph = document.createElement("p");
+  menuParagraph.innerHTML = `The OG is where culinary innovation meets the timeless charm of breakfast and lunch classics. 
+                                Our menu, a treasure trove of gourmet pancakes, artisanal breakfast cocktails, and the best mimosas in Denver, promises an unrivaled dining experience. <br/> <br/>
+                                Savor the flavors of Denver from decadent French toast to savory benedicts, each dish at The OG is crafted to perfection, making us the go-to spot for weekend brunch in Denver. 
+                                Whether you're indulging in a family-friendly brunch or seeking the perfect brunch spot for a lazy Sunday, The OG's unique menu caters to all.`;
+
+  const menuButton = document.createElement("button");
+  menuButton.textContent = "View Menus";
+
+  menuPresentation.id = "menu-presentation";
+  menuPresentation.style.setProperty(
+    `--menu-border`,
+    `url("${menuBorderImg}")`
+  );
+  menuPresentation.appendChild(menuTitle);
+  menuPresentation.appendChild(menuParagraph);
+
+  menuSection.appendChild(menuPresentation);
+  menuButton.style.left = `${menuPresentation.offsetWidth + 230}px `;
+  menuSection.appendChild(menuButton);
+
+  window.addEventListener("resize", () => {
+    menuButton.style.left = `${menuPresentation.offsetWidth + 230}px `;
+  });
+
+  document.addEventListener("scroll", () => {
+    if (window.scrollY > 1255) {
+      menuButton.style.zIndex = "-100";
+    } else {
+      menuButton.style.zIndex = "100";
+    }
+  });
+};
+export default createMenuSection;
